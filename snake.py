b896f9a0156d323cdfa69781d4551f30e04ad289
Jessica McDonald <jessica_mcdonald@brown.edu>
2021-04-06 16:11:11 -0400

    Update snake.py
    Began setup of game

--- snake.py
+++ snake.py
@@ -1,1 +1,12 @@
-print('hi')
+# 4/6: 20 min reading manuals and beginning setup
+
+import pygame
+
+# Import for ability to use keyboard keys
+from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT)
+
+# Initialize
+pygame.init()
+
+# Create display screen
+screen = pygame.display.set_mode([500, 500])
