70013bdc302feac2efba251c560c7128c2d0c71d
rachanachau <79777554+rachanachau@users.noreply.github.com>
2021-04-06 18:52:43 -0400

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -1,1 +1,14 @@
+#4/6: (20 min) Having problems installing pygame + setting up 
 
+import pygame
+
+#Initialize pygame
+pygame.init()
+
+screen = pygame.display.set_mode((576,1024))
+
+
+
+
+#Quit pygame
+pygame.quit()
