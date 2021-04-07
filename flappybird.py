35925513215117dbefd5bf0edfa786d056a76f32
rachanachau <79777554+rachanachau@users.noreply.github.com>
2021-04-06 20:09:45 -0400

    finished background and uploading images
    Finished initial background and uploading future images

--- flappybird.py
+++ flappybird.py
@@ -1,28 +1,46 @@
-#4/6: (45 min) Having problems installing pygame + setting up
-
+# 4/6: (45 min) Having problems installing pygame + setting up
+# 4/6: (1.5 hrs) Finished initial background and uploading future images
 import pygame, sys
 
+#Moving floor function
+def draw_floor():
+    screen.blit(floor, (floor_x_pos, 650))
+    screen.blit(floor, (floor_x_pos + 432, 650))
 
-#Initialize pygame
+
+
+# Initialize pygame
 pygame.init()
 
-screen = pygame.display.set_mode((576,1024))
+screen = pygame.display.set_mode((432, 768))
 clock = pygame.time.Clock()
+
+# Initial background
+background = pygame.image.load('images/fb.images/background-day.png').convert()
+background = pygame.transform.scale(background, (432, 768))
+
+floor = pygame.image.load('images/fb.images/base.png').convert()
+floor = pygame.transform.scale(floor, (432, 150))
+floor_x_pos = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit() #shuts down code
+            sys.exit()  # shuts down code
+
+    screen.blit(background, (0, 0))
+    floor_x_pos += -1
+    draw_floor()
+    if floor_x_pos <= -432:
+        floor_x_pos = 0
+
+
 
     pygame.display.update()
 
-    #baserate speed of the game
+    # base rate speed of the game
     clock.tick(120)
 
-
-
-
-
-#Quit pygame
-#pygame.quit()
+# Quit pygame
+# pygame.quit()
