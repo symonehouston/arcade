5520b4aad882dbf9958b0db27474113d0cf9a8db
Jessica McDonald <jessica_mcdonald@brown.edu>
2021-04-09 11:07:35 -0400

    Update snake.py
    Snake now moves at a constant rate and game ends when edge is hit

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 # 4/6: 20 min reading manuals and beginning setup
         #Getting started: https://realpython.com/pygame-a-primer/
 # 4/6: 2 hr creating a sprite and getting movement
+# 4/9: 1 hr getting constant movement and ending game for hit edge
 
 import pygame
 
@@ -15,16 +16,40 @@ class Snake(pygame.sprite.Sprite):
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
 
+    # Define constant moving function
+    def constant_move(self, x_direction, y_direction):
+        milliseconds = pygame.time.get_ticks()
+        if milliseconds % 100 == 0:
+            self.rect.move_ip(x_direction, y_direction)
+
     # Define moving function
     def move(self, key_pressed):
         if key_pressed[K_UP]:
-            self.rect.move_ip(0, -5)
+            x_direction = 0
+            y_direction = -5
         if key_pressed[K_DOWN]:
-            self.rect.move_ip(0, 5)
+            x_direction = 0
+            y_direction = 5
         if key_pressed[K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            x_direction = -5
+            y_direction = 0
         if key_pressed[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            x_direction = 5
+            y_direction = 0
+        return x_direction, y_direction
+
+    # Define function to check if snake is on screen
+    def on_screen(self):
+        truth = True
+        if self.rect.left < 0:
+            truth = False
+        elif self.rect.right > screen_width:
+            truth = False
+        elif self.rect.top < 0:
+            truth = False
+        elif self.rect.bottom > screen_height:
+            truth = False
+        return truth
 
 # Screen dimensions
 screen_width = 500
@@ -39,27 +64,39 @@ snake = Snake()
 # Create display screen
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+# Set initial direction to the right
+x_direction = 5
+y_direction = 0
+
 # Variable to keep the main loop running
 running = True
 
 # Main loop
 while running:
+    # Quit if snake is off screen
+    if not snake.on_screen():
+        running = False
+
     # for loop through the event queue
     for event in pygame.event.get():
+
         # Check for KEYDOWN event
         if event.type == KEYDOWN:
-            # If the Esc key is pressed, then exit the main loop
+
+            # Change direction according to key pressed
+            key_pressed = pygame.key.get_pressed()
+            x_direction, y_direction = snake.move(key_pressed)
+
+            # Check for ESC key press
             if event.key == K_ESCAPE:
                 running = False
-        # Check for QUIT event. If QUIT, then set running to false.
+
+        # Check for QUIT event
         elif event.type == QUIT:
             running = False
 
-    # Set key pressed
-    key_pressed = pygame.key.get_pressed()
-
-    # Move snake
-    snake.move(key_pressed)
+    # Get constant movement
+    snake.constant_move(x_direction, y_direction)
 
     # Fill the screen with black
     screen.fill((0, 0, 0))
